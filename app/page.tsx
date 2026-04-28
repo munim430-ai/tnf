ffe58061969f5278f6b82f58978cea0770dd5c8e
Munim Hasibul <munimm247@gmail.com>
2026-04-28 11:04:06 +0600

    Load contact number normalizer

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import HomePage from '../components/HomePage'
 import TNFUpgrade from '../components/TNFUpgrade'
+import ContactNumberNormalizer from '../components/ContactNumberNormalizer'
 
 export default function Page() {
   return (
     <>
       <HomePage />
       <TNFUpgrade />
+      <ContactNumberNormalizer />
     </>
   )
 }
